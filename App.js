bdd6218dfb0e7613a6bb7014d29a4f94855c0f2b
Andrew Couperthwaite <acouperthwaite@gmail.com>
2018-07-10 20:33:34 -0600

    Added librarieslist.json and LibraryReducer and LibraryList Component

--- App.js
+++ App.js
@@ -4,12 +4,17 @@ import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 
 import reducers from './src/reducers';
+import { Header } from './src/components/common';
+import LibraryList from './src/components/LibraryList'
 
 class App extends Component {
   render(){
     return (
       <Provider store={createStore(reducers)}>
-        <View />
+        <View>
+          <Header headerText="Tech Stack" />
+          <LibraryList />
+        </View>
       </Provider>
     );
   };
