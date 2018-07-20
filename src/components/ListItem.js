c17852c99a498719adedc4405c182694ce3b5332
Andrew Couperthwaite <acouperthwaite@gmail.com>
2018-07-20 16:52:53 -0600

    added Layout Animation, added renderDescription, enhanced logic in mapStateToProps to give an expanded flag

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,13 +1,36 @@
 import React, {Component} from 'react';
-import {Text, TouchableWithoutFeedback, View} from 'react-native';
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation
+} from 'react-native';
 import {connect} from 'react-redux';
 import {CardSection } from './common';
 import * as actions from '../actions'; //import everything from index.js from actions folder
 
 class ListItem extends Component {
+  componentWillUpdate(){ //lifecycle methods are called automatically- no need to call it :) 
+    LayoutAnimation.spring();
+  }
+
+  renderDescription(){
+    const {library,expanded} = this.props;
+
+    if (expanded){
+      return (
+        <CardSection>
+          <Text style={{flex:1, paddingLeft: 15}}>
+            {library.item.description}
+          </Text>
+        </CardSection>
+      );
+    }
+  }
   render(){
     const { titleStyle } = styles;
     const { id, title} = this.props.library.item;
+
 
     return (
       <TouchableWithoutFeedback
@@ -19,6 +42,7 @@ class ListItem extends Component {
               {title}
             </Text>
           </CardSection>
+          {this.renderDescription()}
         </View>
       </TouchableWithoutFeedback>
     );
@@ -32,6 +56,14 @@ const styles = {
   }
 };
 
-export default connect(null, actions)(ListItem); //first argument is for mapStateToProps, passing null because we're not doing that here
+const mapStateToProps = (state, ownProps) =>{
+
+  const expanded = state.selectedLibraryId === ownProps.library.item.id;
+  return { expanded };
+}
+
+export default connect(mapStateToProps, actions)(ListItem);
+
+//first argument is for mapStateToProps, passing null because we're not doing that here
 // essentially, this say:
 // take the action creators (actions) and whenever they are called the actions go to the right place then pass the actions into the component as props
